a20c698c08f79e230f1a84be56023f4054258a1c
YanSushchynski <yan_sushchynski@outlook.com>
2020-03-09 17:31:21 +0300

    cleanup

--- tests/test_mu.py
+++ tests/test_mu.py
@@ -43,16 +43,6 @@ from c7n.mu import (
     CloudWatchEventSource
 )
 
-for k, v in sorted(sys.modules.items()):
-    print(k, v)
-
-for k in sorted(sys.path):
-    print(k)
-
-import c7n
-
-print(c7n.__path__)
-
 from c7n.ufuncs import logsub
 from .common import (
     BaseTest, event_data, functional, Bag, ACCOUNT_ID)
